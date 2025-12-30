a84b71990be771049c16f105a5f3d114c3010441
hamed kharazmi <hamed.kharazmi@bandpey.com>
2025-12-30 20:40:22 +0100

    created folder structure

---
+++ Flashcards/View/TopicView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Flashcards
+//
+//  Created by Hamed Kharazmi on 30.12.25.
+//
+
+import SwiftUI
+
+struct TopicView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("TODO...")
+            }
+            .navigationTitle("Flashcards")
+        }
+    }
+}
+
+#Preview {
+    TopicView()
+}
